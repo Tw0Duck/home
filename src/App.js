43fdc52b05fc1943e32eade89ee418c3e595eaad
Gustavo-F <gusta.ff230@mail.com>
2022-06-21 01:55:43 -0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>queijo</h1>
+      <Link to='/home'>home</Link>
     </div>
   );
 }
